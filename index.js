1a323d85186a8102dbd7834631d24a51300a6c80
Nawaf Alwisidi <nawafalwisidi@gmail.com>
2022-11-15 20:49:59 +0300

    middleware added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express'),
 	app = express(),
+	cors = require('cors'),
 	greeting = require('./routes/greeting'),
 	PORT = 3732
+
+// Middleware
+app.use(cors())
 
 // TODO: Create api that returns a data string named 'message' contains 'Hello Tuwaiq 😊'
 app.use('/api/greeting', greeting)
