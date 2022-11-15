9f79fbdc59fd98858867f203037a90988255bc99
Nawaf Alwisidi <nawafalwisidi@gmail.com>
2022-11-15 03:16:47 +0300

    Server listener configured

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hi :)')
+const express = require('express'),
+	app = express(),
+	greeting = require('./routes/greeting'),
+	PORT = 3732
+
+// TODO: Create api that returns a data string named 'message' contains 'Hello Tuwaiq 😊'
+app.use('/api/greeting', greeting)
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
