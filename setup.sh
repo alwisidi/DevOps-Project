9a3b3536db627bc2b4af3874c01b7561812c09a0
Nawaf Alwisidi <nawafalwisidi@gmail.com>
2022-11-16 17:21:58 +0300

    Automate setup process script added

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+## Setup the Dev Environment automatically
+git clone https://github.com/alwisidi/tuwaiq_devops_project.git
+npm i --prefix ./server
+npm i --prefix ./client
+
+## To run both the server and the react app in one line
+# node ./server/index.js & cd ./client && npm start
+
+## Build Docker images and run them using Docker Compose
+docker build -t tuwaiq_prj_server ./server &
+docker build -t tuwaiq_prj_client ./client &&
+docker compose up
